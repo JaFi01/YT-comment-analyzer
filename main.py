7e7eb0ddbf671651e944f7220ba994432429f6a9
JaFi01 <116461510+JaFi01@users.noreply.github.com>
2024-03-22 18:05:46 +0100

    improved displaying comments, added like count

--- main.py
+++ main.py
@@ -6,20 +6,30 @@ import re
 def main():
     load_dotenv()
     api_key_yt= os.getenv("YT_API_KEY")
-
-    video_url="https://www.youtube.com/watch?v=WAmEZBEeNmg&ab_channel=AkamaiDeveloper"
+    #video_url = str(input("Podaj adres url filmu"))
+    video_url="https://www.youtube.com/watch?v=p4ZfkezDTXQ&ab_channel=AndrewHuberman"
+    #this video is choosen to test, as feedback is positive.
     id = re.split('=|&' ,video_url)[1]
 
     youtube = build('youtube', 'v3', developerKey=api_key_yt)
 
-    request = youtube.commentThreads().list(part="snippet", videoId=id)
+    request = youtube.commentThreads().list(part="snippet", videoId=id, maxResults=100)
     response = request.execute()
-    print(response)
-    print('\n koniec bazowego response\n')
+    comments_array=[]
     for comment in response["items"]:
         author_name = comment["snippet"]["topLevelComment"]["snippet"]["authorDisplayName"]
-        comment_text = comment["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
-        print(f'{author_name}:  {comment_text} ')
+        comment_text = comment["snippet"]["topLevelComment"]["snippet"]["textOriginal"]
+        like_count = comment["snippet"]["topLevelComment"]["snippet"]["likeCount"]
+
+        striped_comment = [like_count, author_name, comment_text]
+        comments_array.append(striped_comment)
+
+    comments_array.sort(key=lambda x: x[0], reverse=True)
+
+    # Print the comments array
+    for i, stripped_comment in enumerate(comments_array):
+        output = str(stripped_comment[0]) + f" Likes + {stripped_comment[1]}:  {stripped_comment[2]}"
+        print(i+1, ". "+output)
 
 if __name__ == "__main__":
     main()
